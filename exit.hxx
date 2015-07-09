ec873951a216e36d7bc719ff5abc4e9968fda094
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-07-09 06:43:43 -0400

    exit()

---
+++ exit.hxx
@@ -0,0 +1,36 @@
+#pragma once
+
+namespace linux {
+
+[[noreturn]]
+static inline
+void
+exit(int status) noexcept
+{
+#if defined(__arm__)
+#  define __NR_exit 1
+
+    register int r0 asm ("r0") = __NR_exit;
+    register int r1 asm ("r1") = statue;
+
+    asm volatile ("swi 0x0"
+                  :
+                  : "r" (r0)
+                    "r" (r1));
+
+#elif defined(__x86_64__)
+#  define __NR_exit 60
+
+    asm volatile ("syscall"
+                  :
+                  : "a" (__NR_exit),
+                    "D" (status));
+
+#else
+#  error
+#endif
+
+    __builtin_unreachable();
+}
+
+}
